84608a5c9ac7477e07bcb1b04f78be20763b422b
SUMIT SIROHI <88738447+S78648@users.noreply.github.com>
2026-02-06 16:08:02 +0530

    Add files via upload

---
+++ management/src/main/java/com/calorie/management/CalcfitApplication.java
@@ -0,0 +1,13 @@
+package com.calorie.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcfitApplication.class, args);
+	}
+
+}
---
+++ management/src/test/java/com/calorie/management/CalcfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calorie.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
